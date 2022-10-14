008ddb5a688951dd3b4fa0791fd919bf6fa74971
Linus Thorsell <linth181@student.liu.se>
2022-10-14 09:58:40 +0200

    added dialog to city entry

--- GuardGeneric.gd
+++ GuardGeneric.gd
@@ -8,6 +8,7 @@ export var default_direction = "standing_down"
 signal signal_other_guard
 
 var player_node
+var return_after_move = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,19 +23,41 @@ func _ready():
 
 
 func _on_GuardsAtIntro_area_entered(area):
-	$AnimatedSprite.play("standing_up")
-	$SpeechBubble.set_text("STOP! Who are you?!", 5)
-	yield(get_tree().create_timer(5.5),"timeout")
-	var selection = yield(player_node.ask_player("STOP! Who are you?!", ["Not a skelly!", "I'm not so sure actually...", "My names guard", "Uhm..."]), "completed")
-	print(selection)
-	emit_signal("signal_other_guard")
-	yield(get_tree().create_timer(11),"timeout")
-	$SpeechBubble.set_text("Exactly...", 5)
-
+	if (area.name == "PlayerCollision"):
+		area.get_parent().locked_in_place = true;
+		$AnimatedSprite.play("standing_up")
+		$SpeechBubble.set_text("STOP! Who are you?!", 5)
+		yield(get_tree().create_timer(5.5),"timeout")
+		var selection = yield(player_node.ask_player("STOP! Who are you?!", ["Not a skelly!", "I'm not so sure actually...", "My names guard", "Uhm..."]), "completed")
+		emit_signal("signal_other_guard")
+		yield(get_tree().create_timer(11),"timeout")
+		$SpeechBubble.set_text("Exactly...", 5)
+		yield(get_tree().create_timer(3),"timeout")
+		$SpeechBubble.set_text("Oh my! Come here Player", 1)
+		
+		return_after_move = 1;
+		$NPCMoveEngine.move_node_along_path($".", $City_CollectPlayer1/Follow, 150)
 
 func _on_GuardGenericLeftGate_signal_other_guard():
 	$AnimatedSprite.play("standing_up")
-	$SpeechBubble.set_text("Why should we let you in here...", 5)
+	$SpeechBubble.set_text("Sounds like exactly what a skelly would say...", 5)
 	yield(get_tree().create_timer(5.0),"timeout")
-	$SpeechBubble.set_text("what if you are a skelly in disguise?", 5)
+	$SpeechBubble.set_text("You are deffo a skelly arent you...", 5)
+	yield(get_tree().create_timer(5.0),"timeout")
+	
+	yield(get_tree().create_timer(3),"timeout")
+	$SpeechBubble.set_text("HEY WATCH OUT!!!", 2)
+	
+	$NPCMoveEngine.move_node_along_path($".", $City_ProtectPlayer/Follow, 150);
+	#pass # Replace with function body.
+
+
+func _on_NPCMoveEngine_finished_moving():
+	if (return_after_move == 1):
+		return_after_move = 2;
+		$NPCMoveEngine.move_node_along_path($".", $City_CollectPlayer2/Follow, 100)
+	elif (return_after_move == 2):
+		return_after_move = 3;
+		$NPCMoveEngine.move_node_along_path($".", $City_CollectPlayer3/Follow, 150)
+		
 	#pass # Replace with function body.
---
+++ NPCMoveEngine.gd
@@ -0,0 +1,59 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var should_move = false;
+var target = null;
+var forceplayer = null;
+var path = null;
+var speed = null;
+var last_location = -1;
+
+signal finished_moving
+
+func move_node_if_active(delta):
+	if (should_move):
+#		print("moving")
+#		print(target)
+#		print(target.position)
+#		print(path.get_offset())
+#		print(path.position)
+#		print("======")
+#		print("moving")
+		if (path.get_offset() == last_location):
+			# stop condition
+			print("stopping")
+			should_move = false
+			emit_signal("finished_moving")
+		
+		last_location = path.get_offset()
+		path.set_offset(path.get_offset() + speed * delta)
+		
+		target.position = path.position;
+	
+func move_node_along_path(target_node, follow_path, follow_speed, force_playerfollow=false):
+	print("walking: node, path, speed")
+	print(target_node)
+	print(follow_path)
+	print(follow_speed)
+	print(force_playerfollow)
+	print("=========")
+	target = target_node;
+	path = follow_path;
+	speed = follow_speed;
+	forceplayer = force_playerfollow
+	
+	should_move = true;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move_node_if_active(delta)
+#	pass
